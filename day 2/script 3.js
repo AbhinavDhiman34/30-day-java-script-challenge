6fe4adce6d009e55b7065cc32d923632ee198f3f
Abhinav Dhiman <166062355+AbhinavDhiman34@users.noreply.github.com>
2024-07-18 02:09:49 +0530

    Add files via upload

---
+++ day 2/script 3.js
@@ -0,0 +1,34 @@
+                         //activity 4 logical operators //task 11 // a program that uses  && condition
+let a = 5 ;
+let b =5;
+let c=10;
+let d =10;
+if (a===b && c===d) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+//task 12 program that uses || condition
+let e = 6 ;
+let f =5;
+let g=15;
+let h =10;
+if (e===f || g===h) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+//task 13 program that uses ! condition 
+let i = 6 ;
+let j =5;
+
+if (i!=j) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+                              //activity 5 ternary operatrot
+                              // Define a number
+let number = -10;
+let result = number >= 0 ? number + " is positive" : number + " is not positive";
+console.log(result);
---
+++ day 2/script2.js
@@ -0,0 +1,93 @@
+//activity 1-------------------------------------------------------------------------------------------------------------
+//for addition task 1
+let a = 5;
+let b = 100;
+sum = a+b;
+//for substraction  task 2
+console.log(sum);
+let c = 5;
+let d = 100;
+sub = c-d;
+console.log(sub);
+//for multiplication task 3
+let e = 5;
+let f = 100;
+mul = e*f;
+console.log(mul);
+//division task 4
+let g = 5;
+let h = 100;
+div = g/h;
+console.log(div);
+//remainder task 5
+let i = 100;
+let j = 5;
+rem = i%j;
+console.log(rem);
+//activity2--------------------------------------------------------------------------------------------------------------
+//using +=operatot  task 6
+let k =5;
+let l =15;
+k+=l;
+console.log(k);
+// using -= operator task 7 
+let m=20;
+let n=15;
+m-=n;
+console.log(m);
+//activity 3 comparision operator---------------------------------------------------------------------------------------------
+//task 8 using <
+let o =5;
+let p = 10;
+if (o<p) {
+    console.log("o is smaller than p");
+} else {
+    console.log("o is greater than p");
+}
+//using >
+let q =20;
+let r = 10;
+if (q>r) {
+    console.log("r is smaller than q");
+} else {
+    console.log("o is greater than p");
+}
+//task 9 using >= 
+let s =10;
+let t = 10;
+if(s>=t){
+    console.log("s is greater than or equal to t");
+
+}
+else{
+    console.log("s is smaller than t");
+
+}
+// using <=
+let u =10;
+let v = 10;
+if(u<=t){
+    console.log("u is greater than or equal tov");
+
+}
+else{
+    console.log("u is smaller than v");
+
+}
+// task 10 using ==
+let w = 5;
+let x = 5.0;
+if (w==x) {
+   console.log("w and x are equal");
+} else {
+    console.log("w and x are not equal");
+}
+
+//using ===
+let y = 5;
+let z = 5.00;
+if (y === z) {
+   console.log("y and z are strictly equal");
+} else {
+    console.log("y and z are strictly not equal");
+}
